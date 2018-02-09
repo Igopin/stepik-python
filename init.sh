ba2c482276f7625ec30e19050bd656b0e56a52a7
Igor Pinaev <igor.pinaev@novartis.com>
2018-02-09 17:41:46 +0300

    updated init script

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo rm -f /etc/nginx/sites-enabled/*
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.hello.conf   /etc/gunicorn.d/hello.conf
+sudo ln -s /home/box/web/etc/gunicorn.django.conf   /etc/gunicorn.d/django.conf
 sudo /etc/init.d/gunicorn restart
